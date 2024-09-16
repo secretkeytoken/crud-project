50dc7210ab2e85a202407b6e29d357078ce86dfe
leodevai <leopham.dev@gmail.com>
2024-09-16 10:27:53 +0700

    setup theme code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Button>default</Button>
+        <Button variant={"destructive"}>destructive</Button>
+        <Button variant={"secondary"}>secondary</Button>
+        <Button variant={"ghost"}>ghost</Button>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
